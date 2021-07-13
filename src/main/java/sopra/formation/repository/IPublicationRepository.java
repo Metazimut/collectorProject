6af089a5ab4e7dce55b49e1fe13d47d81c806c27
Aurélien <aurelien.dispot@gmail.com>
2021-07-13 16:16:35 +0200

    Repository init

---
+++ src/main/java/sopra/formation/repository/IPublicationRepository.java
@@ -0,0 +1,7 @@
+package sopra.formation.repository;
+
+import sopra.formation.model.Publication;
+
+public interface IPublicationRepository extends IRepository<Publication, Long>{
+
+}
---
+++ src/main/java/sopra/formation/repository/jpa/CategorieRepositoryJpa.java
@@ -0,0 +1,126 @@
+package sopra.formation.repository.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.formation.Application;
+import sopra.formation.model.Categorie;
+import sopra.formation.repository.ICategorieRepository;
+
+public class CategorieRepositoryJpa implements ICategorieRepository {
+	public List<Categorie> findAll() {
+		List<Categorie> categories = new ArrayList<Categorie>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Categorie> query = em.createQuery("select f from Categorie f", Categorie.class);
+
+			categories = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return categories;
+	}
+
+	public Categorie findById(Long id) {
+		Categorie categorie = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			categorie = em.find(Categorie.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return categorie;
+	}
+
+	public Categorie save(Categorie obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	public void delete(Categorie obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+}
