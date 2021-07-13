ad955b0bea5a5d13bc5bd429dc6e5d6f4494348c
Aurélien <aurelien.dispot@gmail.com>
2021-07-13 16:21:44 +0200

    Application init

--- src/main/java/sopra/formation/Application.java
+++ src/main/java/sopra/formation/Application.java
@@ -3,15 +3,21 @@ package sopra.formation;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import sopra.formation.repository.IAdresseRepository;
-import sopra.formation.repository.jpa.AdresseRepositoryJpa;
+import sopra.formation.repository.ICategorieRepository;
+import sopra.formation.repository.IParticipationEnchereRepository;
+import sopra.formation.repository.IPublicationRepository;
+import sopra.formation.repository.jpa.CategorieRepositoryJpa;
+import sopra.formation.repository.jpa.ParticipationEnchereRepositoryJpa;
+import sopra.formation.repository.jpa.PublicationRepositoryJpa;
 
 
 public class Application {
 	private static Application instance = null;
 
 	private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("formation-jpa");
-
+	private final ICategorieRepository categorieRepo = new CategorieRepositoryJpa();
+	private final IParticipationEnchereRepository participationEnchereRepo = new ParticipationEnchereRepositoryJpa();
+	private final IPublicationRepository publicationRepo = new PublicationRepositoryJpa();
 	//private final IEvaluationRepository evaluationRepo = new EvaluationRepositoryJpa();
 
 
@@ -29,5 +35,18 @@ public class Application {
 	public EntityManagerFactory getEntityManagerFactory() {
 		return entityManagerFactory;
 	}
+
+	public ICategorieRepository getCategorieRepo() {
+		return categorieRepo;
+	}
+
+	public IParticipationEnchereRepository getParticipationEnchereRepo() {
+		return participationEnchereRepo;
+	}
+
+	public IPublicationRepository getPublicationRepo() {
+		return publicationRepo;
+	}
+	
 
 }
