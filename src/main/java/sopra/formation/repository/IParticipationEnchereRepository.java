72cb14f016c83c06edb7b4136ddd7af04af1a136
Mathieu W <w.mathieu.dk59@gmail.com>
2021-07-20 14:04:25 +0200

    springification

--- src/main/java/sopra/formation/repository/IParticipationEnchereRepository.java
+++ src/main/java/sopra/formation/repository/IParticipationEnchereRepository.java
@@ -1,7 +1,7 @@
 package sopra.formation.repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import sopra.formation.model.ParticipationEnchere;
 
-public interface IParticipationEnchereRepository extends IRepository<ParticipationEnchere, Long> {
+public interface IParticipationEnchereRepository extends JpaRepository<ParticipationEnchere, Long> {
 
 }
--- src/main/java/sopra/formation/repository/IPublicationRepository.java
+++ src/main/java/sopra/formation/repository/IPublicationRepository.java
@@ -1,7 +1,7 @@
 package sopra.formation.repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import sopra.formation.model.Publication;
 
-public interface IPublicationRepository extends IRepository<Publication, Long>{
+public interface IPublicationRepository extends JpaRepository<Publication, Long>{
 
 }
