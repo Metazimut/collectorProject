894592dc6a26d2cba1d051b83d4762e225d5e144
Mathieu W <w.mathieu.dk59@gmail.com>
2021-07-20 15:59:32 +0200

    firstTest

--- src/main/java/sopra/formation/repository/ICompteRepository.java
+++ src/main/java/sopra/formation/repository/ICompteRepository.java
@@ -1,7 +1,14 @@
 package sopra.formation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.formation.model.Commentaire;
 import sopra.formation.model.Compte;
+import sopra.formation.model.ParticipationEnchere;
+import sopra.formation.model.Publication;
+import sopra.formation.model.Utilisateur;
 
 public interface ICompteRepository extends JpaRepository<Compte, Long> {
-
+	Utilisateur findByEncheres(ParticipationEnchere enchere);
+	Utilisateur findByPublications(Publication publication);
+	Utilisateur findByCommentaires(Commentaire commentaire);
 }
