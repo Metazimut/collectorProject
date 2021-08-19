4b9d82b813d3fdf560090a8bc08080130c59152e
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-19 11:46:02 +0200

    restcontroller admin utilisateur adresse

---
+++ collectorProjectBoot/src/main/java/sopra/formation/model/Views.java
@@ -0,0 +1,16 @@
+package sopra.formation.model;
+
+import sopra.formation.model.Views.ViewCommon;
+
+public class Views {
+	public static class ViewCommon {}
+	
+	public static class ViewCompte extends ViewCommon {}
+	
+	public static class ViewAdmin extends ViewCompte {}
+
+	public static class ViewUtilisateur extends ViewCompte {}
+	
+	public static class ViewAdresse extends ViewCommon {}
+}
+
--- collectorProjectOld/src/main/java/sopra/formation/repository/ICompteRepository.java
+++ collectorProjectOld/src/main/java/sopra/formation/repository/ICompteRepository.java
@@ -1,5 +1,6 @@
 package sopra.formation.repository;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,11 +8,26 @@ import org.springframework.data.repository.query.Param;
 
 import sopra.formation.model.Commentaire;
 import sopra.formation.model.Compte;
+import sopra.formation.model.Utilisateur;
 import sopra.formation.model.ParticipationEnchere;
 import sopra.formation.model.Publication;
-import sopra.formation.model.Utilisateur;
+import sopra.formation.model.Admin;
+
 
 public interface ICompteRepository extends JpaRepository<Compte, Long> {
+	
+	@Query("select s from Admin s")
+	List<Admin> findAllAdmin(); // @Query
+
+	@Query("select f from Utilisateur f")
+	List<Utilisateur> findAllUtilisateur(); // @Query
+	
+	@Query("select s from Admin s where s.id = :id")
+	Optional<Admin> findAdminById(@Param("id") Long id);
+	
+	@Query("select f from Utilisateur f where f.id = :id")
+	Optional<Utilisateur> findUtilisateurById(@Param("id") Long id);
+	
 	Utilisateur findByEncheres(ParticipationEnchere enchere);
 	Utilisateur findByPublications(Publication publication);
 	Utilisateur findByCommentaires(Commentaire commentaire);
