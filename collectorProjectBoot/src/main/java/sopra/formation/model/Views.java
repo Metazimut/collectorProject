7ad35256f3d111220a19051322a80dd2214e8f32
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-19 11:48:42 +0200

    clean view

--- collectorProjectBoot/src/main/java/sopra/formation/model/Views.java
+++ collectorProjectBoot/src/main/java/sopra/formation/model/Views.java
@@ -1,7 +1,5 @@
 package sopra.formation.model;
 
-import sopra.formation.model.Views.ViewCommon;
-
 public class Views {
 	public static class ViewCommon {}
 	
@@ -13,8 +11,10 @@ public class Views {
 	
 	public static class ViewAdresse extends ViewCommon {}
 
-
 	public static class ViewPublication extends ViewCommon {}
+	
 	public static class ViewParticipationEnchere extends ViewCommon {}
+	
 	public static class ViewCategorie extends ViewCommon {}
+	
 }
